abd566680df47e3b342074ef389eacbda7375ed6
Mjtala <mjtala@uc.cl>
2022-01-12 13:41:36 -0300

    feat(models): tablas intermedias de recipe

--- apps/recipe/models/recipe.py
+++ apps/recipe/models/recipe.py
@@ -15,7 +15,27 @@ class Recipe(models.Model):
     owner = models.ForeignKey('user.User', on_delete=models.CASCADE,
                               related_name='owner')
 
+    notes = models.ManyToManyField(
+        'user.User', through='recipeNotes', through_fields=('recipe_id', 'user_id'))
+
+    notes = models.ManyToManyField(
+        'user.User', through='LastTimeCooked', through_fields=('recipe_id', 'user_id'))
+
     def __str__(self):
         return self.name
 
 
+class RecipeNotes(models.Model):
+    recipe_id = models.ForeignKey(
+        'recipe.Recipe', on_delete=models.CASCADE, related_name='recipe_id_recipeNotes')
+    user_id = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, related_name='user_id_recipeNotes')
+    note = models.TextField(max_length=200)
+
+
+class LastTimeCooked(models.Model):
+    recipe_id = models.ForeignKey(
+        'recipe.Recipe', on_delete=models.CASCADE, related_name='recipe_id_lastTimeCooked')
+    user_id = models.ForeignKey(
+        'user.User', on_delete=models.CASCADE, related_name='user_id_lastTimeCooked')
+    lastTimeCooked = models.PositiveIntegerField()
--- apps/shopList/models/shopList.py
+++ apps/shopList/models/shopList.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class ShopList(models.Model):
     is_completed = models.BooleanField(blank=True, null=True)
-    # ingredient = models.ForeignKey('shopList.ShopList.', on_delete=models.CASCADE,
-    # related_name='ingredient')
+    # ingredient_id_shopList = models.ForeignKey('ingredient.Ingredient', on_delete=models.CASCADE,
+    #                                            related_name='ingredient_id_shopList')
 
     # def __str__(self):
     #     return self.name
