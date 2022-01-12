36596222daaa00ebce5b3f105b89499361db4839
Mjtala <mjtala@uc.cl>
2022-01-12 13:50:22 -0300

    feat(models): tablas intermedias de shoplist listas

--- apps/ingredient/models/ingredient.py
+++ apps/ingredient/models/ingredient.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class Ingredient(models.Model):
@@ -6,5 +7,20 @@ class Ingredient(models.Model):
     category = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
         return self.name
+
+
+class IngredientShopList(Ingredient):
+    shopList_id = models.ForeignKey(
+        'shopList.ShopList', on_delete=CASCADE, related_name='shoplist_id_ingredientShopList')
+    amount = models.FloatField()
+
+
+class IngredientRecipe(Ingredient):
+    recipe_id = models.ForeignKey(
+        'recipe.Recipe', on_delete=CASCADE, related_name='recipe_id_ingredientRecipe')
+    amount = models.FloatField()
