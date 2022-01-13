f48dcde36e57a5d0496abe366bfee1a59f6a4cdc
Mjtala <mjtala@uc.cl>
2022-01-13 17:44:21 -0300

    fix(shoplist): fix pr comments about shoplist and ingredient table

--- apps/ingredient/models/ingredient.py
+++ apps/ingredient/models/ingredient.py
@@ -6,18 +6,20 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
+    shop_list = models.ManyToManyField(
+        through='ingredientShopList.IngredientShopList', related='ingredients')
 
     def __str__(self):
         return self.name
 
 
-class IngredientShopList(Ingredient):
+class IngredientShopList(models.Model):
     shopList_id = models.ForeignKey(
         'shopList.ShopList', on_delete=CASCADE, related_name='shoplist_id_ingredientShopList')
     amount = models.FloatField()
 
 
-class IngredientRecipe(Ingredient):
+class IngredientRecipe(models.Model):
     recipe_id = models.ForeignKey(
         'recipe.Recipe', on_delete=CASCADE, related_name='recipe_id_ingredientRecipe')
     amount = models.FloatField()
--- apps/shopList/models/shopList.py
+++ apps/shopList/models/shopList.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class ShopList(models.Model):
     is_completed = models.BooleanField(blank=True, null=True)
-    # ingredient_id = models.ManyToManyField(
-    #     'ingredientShopList.IngredientShopList')
+    ingredient_id = models.ManyToManyField(
+        through='ingredient.IngredientShopList', related_name='shop_lists'
+    )
 
     def __str__(self):
         return self.id
