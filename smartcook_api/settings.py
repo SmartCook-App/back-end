f6e0a4aeac592d0561c4c46fe1ea88c6d7f82b52
msmendoza <msmendoza@uc.cl>
2022-01-15 11:28:40 -0300

    feat(common): added django-extensions

--- smartcook_api/settings.py
+++ smartcook_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'phonenumber_field',
     'apps.user.apps.UserConfig',
     'apps.recipe.apps.RecipeConfig',
