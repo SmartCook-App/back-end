9c7bd99e4effe56ef7cb3a624be03b464f864f66
fivaldivia <fivaldivia@uc.cl>
2022-02-22 16:10:55 -0300

    feat(serializer):recipes serializer modified to send id

--- apps/recipe/serializers/serializers.py
+++ apps/recipe/serializers/serializers.py
@@ -5,5 +5,5 @@ from ..models.recipe import Recipe
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
-        fields = ['name', 'time', 'tips', 'image', 'calories', 'fat',
+        fields = ['id', 'name', 'time', 'tips', 'image', 'calories', 'fat',
                   'proteins', 'carbs', 'difficulty', 'portions', 'owner']
